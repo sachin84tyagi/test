06460310e19ab8a8af404928fdc34e5372f6bb06
sachin84tyagi <sachin84tyagi@gmail.com>
2016-05-07 03:13:57 +0530

    changes 9
    changes 9

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
 echo "<pre>";
 print_r($_SERVER);
-echo "</pre>";
-echo "$_SERVER";
-echo "</pre>";
-echo "</pre>";
-echo "</pre>";
-echo "$_SERVER";
-echo "</pre>";
-echo "</pre>";
-echo "</pre>";
-echo "$_SERVER";
-echo "</pre>";
-echo "</pre>";
-echo "</pre>";
-echo "$_SERVER";
-
 ?>
