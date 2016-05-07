5c1ff5c771787e6de387bbf72f5692cb5753923e
Sachin Tyagi <sachin84tyagi@gmail.com>
2016-05-07 06:02:57 +0530

    changes443u7

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER111);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER222);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER333);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER444);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER555);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER666);
 echo "</pre>";
 
 ?>
