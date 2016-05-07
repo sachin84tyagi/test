c9a0588b1b133d29715631270330773b41c850e3
Sachin Tyagi <sachin84tyagi@gmail.com>
2016-05-08 01:33:43 +0530

    changes

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER6666);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER777777777);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER88888888888888);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER99999999999);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVER900000000000000000000000);
 echo "</pre>";
 
 echo "<pre>";
-print_r($_SERVER);
+print_r($_SERVERppppppppppppppppp);
 echo "</pre>";
 
 ?>
